e871b42215c5caeec2cd4836362d54e3e8faa0b4
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-01-15 17:51:32 +0300

    Пофиксил механиков

--- Models/Mechanic.cs
+++ Models/Mechanic.cs
@@ -1,14 +1,24 @@
-﻿namespace repairshop_client.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace repairshop_client.Models;
+
+[Table("mechanics")]
 public class Mechanic
 {
+	[Key]
+	[Column("mechanic_id")]
 	public Int16 MechanicId { get; set; }
 
+	[Column("name")]
 	public string Name { get; set; }
 
+	[Column("surname")]
 	public string Surname { get; set; }
 
+	[Column("patronim")]
 	public string? Patronim { get; set; }
 
+	[Column("qualification")]
 	public Int16 Qualification { get; set; }
 }
