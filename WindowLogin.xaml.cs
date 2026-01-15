063f05e9b56208cdb86d94327e53ae99a3d21e2c
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-01-15 15:10:00 +0300

    Сделал взаимодействие окон

---
+++ WindowLogin.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace repairshop_client;
+
+/// <summary>
+/// Interaction logic for WindowLogin.xaml
+/// </summary>
+public partial class WindowLogin : Window
+{
+	public string Hostname => this.TextBoxHostname.Text;
+
+	public string Login => this.TextBoxLogin.Text;
+
+	public string Password => this.PasswordBox.Password;
+
+	public WindowLogin(string hostname, string login, string password)
+	{
+		InitializeComponent();
+		this.TextBoxHostname.Text = hostname;
+		this.TextBoxLogin.Text = login;
+		this.PasswordBox.Password = password;
+	}
+
+	private void ButtonOK_Click(object sender, RoutedEventArgs e)
+	{
+		this.DialogResult = true;
+		this.Close();
+	}
+}
--- WindowMain.xaml.cs
+++ WindowMain.xaml.cs
@@ -7,6 +7,42 @@ namespace repairshop_client;
 /// </summary>
 public partial class WindowMain : Window
 {
+	private string hostname = "localhost";
+	private string login = "";
+	private string password = "";
+
 	public WindowMain() => this.InitializeComponent();
+	private void Window_Loaded(object sender, RoutedEventArgs e)
+	{
+		if (this.Auth()) { 
+			this.WindowState = WindowState.Normal;
+		}
+		else {
+			this.Close();
+		}
+	}
+
+	private bool Auth ()
+	{
+		var windowLogin = new WindowLogin(hostname, login, password);
+		if (windowLogin.ShowDialog() ?? false) {
+			this.Tabs.IsEnabled = true;
+			MessageBox.Show($"windowLogin true:{windowLogin.Hostname}\r\n{windowLogin.Login} {windowLogin.Password}");
+			return true;
+		}
+		else {
+			MessageBox.Show("windowLogin false");
+			this.Tabs.IsEnabled = false;
+			return false;
+		}
+	}
+
+	private void ButtonAuthClick(object sender, RoutedEventArgs e) => this.Auth();
+
+	private void ButtonCloseClick(object sender, RoutedEventArgs e)
+	{
+		this.Close();
+	}
+
 }
 
