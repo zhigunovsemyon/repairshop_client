564476e891be224b58c1f7fd7bb5a90c406ce4ae
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-01-15 17:48:40 +0300

    Пофиксил модель клиентов

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,16 +1,27 @@
-﻿namespace repairshop_client.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace repairshop_client.Models;
+
+[Table("clients")]
 public class Client
 {
+	[Key]
+	[Column("client_id")]
 	public Int16 ClientId { get; set; }
 
+	[Column("name")]
 	public string Name { get; set; }
 	
+	[Column("surname")]
 	public string Surname { get; set; }
 
+	[Column("patronim")]
 	public string? Patronim { get; set; }
 
+	[Column("email")]
 	public string? Email { get; set; }
 
+	[Column("phone")]
 	public Int64 Phone {  get; set; }
 }
