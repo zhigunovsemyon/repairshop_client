61503e694d61c440313f2d91a9bb9849acdaf2c9
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-01-15 17:57:42 +0300

    Пофиксил машины

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,17 +1,27 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace repairshop_client.Models;
 
+[Table("car")]
 public class Car
 {
+	[Key]
+	[Column("car_id")]
 	public Int16 CarId { get; set; }
 
+	[Column("plate")]
 	public string Plate { get; set; }
 
+	[Column("brand")]
 	public string Brand { get; set; }
 
+	[Column("model")]
 	public string Model { get; set; }
 	
-	public Int32 Year { get; set; }
+	[Column("year")]
+	public Int32? Year { get; set; }
 
+	[Column("client_id")]
 	public Int16 ClientId { get; set; }
 }
