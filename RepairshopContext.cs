a38a7d71f172fd5e6abe2b4af81e6bdbfefef7fb
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-01-15 22:50:25 +0300

    Добавил лейзилоад

--- RepairshopContext.cs
+++ RepairshopContext.cs
@@ -2,32 +2,25 @@
 
 namespace repairshop_client;
 
-public class RepairshopContext : DbContext
+public class RepairshopContext (string connectionString) : DbContext
 {
-	public DbSet<Models.Car> Cars { get; set; }
+	public DbSet<Models.Car> Cars => Set<Models.Car>();
 
-	public DbSet<Models.Mechanic> Mechanics { get; set; }
+	public DbSet<Models.Mechanic> Mechanics => Set<Models.Mechanic>();
 
-	public DbSet<Models.Service> Services { get; set; }
+	public DbSet<Models.Service> Services => Set<Models.Service>();
 
-	public DbSet<Models.Client> Clients { get; set; }
+	public DbSet<Models.Client> Clients => Set<Models.Client>();
 
-	public DbSet<Models.Repair> Repairs { get; set; }
+	public DbSet<Models.Repair> Repairs => Set<Models.Repair>();
 
-	private readonly string connectionString;
+	private readonly string connectionString = connectionString;
 
-	public RepairshopContext(string connectionString)
-	{
-		this.connectionString = connectionString;
-	}
+	protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) 
+		=> optionsBuilder
+			.UseNpgsql(connectionString)
+			.UseLazyLoadingProxies();
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-	{
-		optionsBuilder.UseNpgsql(connectionString);
-	}
-
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.HasDefaultSchema("repairshop");
-	}
+	protected override void OnModelCreating (ModelBuilder modelBuilder) 
+		=> modelBuilder.HasDefaultSchema("repairshop");
 }
